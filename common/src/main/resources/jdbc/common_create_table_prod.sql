80038fdd293d5a4ca16fdb9df50abdc2f4090b26
twuillemin <thomas.wuillemin@gmail.com>
2019-05-22 22:35:02 +0300

    Add scripts for creating database in PostgreSQL

---
+++ common/src/main/resources/jdbc/common_create_table_prod.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS jds_group
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS jds_user
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR NOT NULL,
+    password   VARCHAR NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name  VARCHAR NOT NULL,
+    enabled    BOOLEAN NOT NULL,
+    profile    VARCHAR NOT NULL
+);
+
+CREATE TABLE  IF NOT EXISTS jds_group_user
+(
+    group_id BIGINT  NOT NULL REFERENCES jds_group (id),
+    user_id  BIGINT  NOT NULL REFERENCES jds_user (id),
+    is_admin BOOLEAN NOT NULL,
+    PRIMARY KEY (group_id, user_id)
+);
